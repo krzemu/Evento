7c09615c890699558ad41b2785680521caf41a56
krzemu <ltryczynski1@gmail.com>
2024-09-19 00:16:17 +0200

    commit

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function Container({ children }: ChildrenNodeProps) {
   return (
-    <div className="max-w-7xl mx-auto bg-white/[2%] min-h-screen flex flex-col">{children}</div>
+    <div className="max-w-7xl mx-auto bg-white/[2%] min-h-screen flex flex-col ">{children}</div>
   );
 }
